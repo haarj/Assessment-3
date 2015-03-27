da3a720babda9a6700d0ca7cf737d3aaa09dd9fc
Justin Haar <justin.haar22@gmail.com>
2015-03-27 13:08:52 -0700

    Initial Commit

---
+++ StationsListViewController.m
@@ -0,0 +1,37 @@
+//
+//  StationsListViewController.m
+//  CodeChallenge3
+//
+//  Created by Vik Denic on 10/16/14.
+//  Copyright (c) 2014 Mobile Makers. All rights reserved.
+//
+
+#import "StationsListViewController.h"
+
+@interface StationsListViewController ()
+
+@end
+
+@implementation StationsListViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
